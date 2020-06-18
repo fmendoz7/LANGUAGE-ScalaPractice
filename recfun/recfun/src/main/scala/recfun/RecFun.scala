fe22eace9941d8a585491b6f2fe8e27d68eda7ae
fmendoz7 <francissamuelmendoza7@gmail.com>
2020-06-17 23:19:52 -0700

    Understood tail recursion for summing coins to value

--- recfun/recfun/src/main/scala/recfun/RecFun.scala
+++ recfun/recfun/src/main/scala/recfun/RecFun.scala
@@ -12,23 +12,18 @@ object RecFun extends RecFunInterface {
       println()
     }
   }
-
-  /**
-   * Exercise 1
-   */
+  //-----------------------------------------------------------------------------------------
+  /** Exercise 1 */
   //(!!!) YOU DID need tail recursion. Constructing traingle
   def pascal(c: Int, r: Int): Int =
-    //Do not need tail recursion here
       //#1: Base Case as you whittle down to 0,0 base case
         //r == 0 is redundant, as only the top element is covered. Covered by c == r
       if(c == 0 || c == r || r ==0 ) 1
 
       //#2: Handle the EDGES
       else pascal(c, r-1) + pascal(c-1, r-1)
-
-  /**
-   * Exercise 2
-   */
+  //-----------------------------------------------------------------------------------------
+  /** Exercise 2 */
   def balance(chars: List[Char]): Boolean =
     //Leetcode similarity. Use a stack.
     var checkStack = Stack[Char]()
@@ -39,9 +34,18 @@ object RecFun extends RecFunInterface {
 
     if(checkStack.isEmpty) true
     else false
+  //-----------------------------------------------------------------------------------------
+  /** Exercise 3 */
+  def countChange(money: Int, coins: List[Int]): Int = {
+    //Using tail recursion to iterate through different heads and tails of lists
 
-  /**
-   * Exercise 3
-   */
-  def countChange(money: Int, coins: List[Int]): Int = ???
+    def recursiveCounter(reCount: Int, attemptList: List[Int]): Int = {
+      if (reCount == money) 1
+      else if (reCount > money || attemptList.isEmpty) 0
+      else recursiveCounter(reCount + attemptList.head, attemptList) + recursiveCounter(reCount, attemptList.tail)
+    }
+
+    if (money == 0) 0
+    else recursiveCounter(0, coins)
+  }
 }
