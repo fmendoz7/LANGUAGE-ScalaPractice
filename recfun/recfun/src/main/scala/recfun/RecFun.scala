42c3e4cc594c9794ef066d087d3d7f5492b21ed3
fmendoz7 <francissamuelmendoza7@gmail.com>
2020-06-17 18:55:39 -0700

    Tail Recursion example #1

--- recfun/recfun/src/main/scala/recfun/RecFun.scala
+++ recfun/recfun/src/main/scala/recfun/RecFun.scala
@@ -6,6 +6,7 @@ object RecFun extends RecFunInterface {
     println("Pascal's Triangle")
     for (row <- 0 to 10) {
       for (col <- 0 to row)
+        //Column, row
         print(s"${pascal(col, row)} ")
       println()
     }
@@ -14,12 +15,21 @@ object RecFun extends RecFunInterface {
   /**
    * Exercise 1
    */
-  def pascal(c: Int, r: Int): Int = ???
+  //(!!!) YOU DID need tail recursion. Constructing traingle
+  def pascal(c: Int, r: Int): Int =
+    //Do not need tail recursion here
+      //#1: Base Case as you whittle down to 0,0 base case
+        //r == 0 is redundant, as only the top element is covered. Covered by c == r
+      if(c == 0 || c == r || r ==0 ) 1
+
+      //#2: Handle the EDGES
+      else pascal(c, r-1) + pascal(c-1, r-1)
 
   /**
    * Exercise 2
    */
   def balance(chars: List[Char]): Boolean = ???
+    //Leetcode similarity. Use a stack.
 
   /**
    * Exercise 3
