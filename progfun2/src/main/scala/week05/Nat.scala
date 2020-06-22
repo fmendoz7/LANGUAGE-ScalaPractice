1d712e69c0deecbd90df77bd472888c8e7b9e768
fmendoz7 <francissamuelmendoza7@gmail.com>
2020-06-22 03:38:51 -0700

    Fixed Succ - infix and added edge case if zero

--- progfun2/src/main/scala/week05/Nat.scala
+++ progfun2/src/main/scala/week05/Nat.scala
@@ -1,4 +1,5 @@
 //package week05
+//Understand how Peano numbers work
 
 abstract class Nat {
   def isZero: Boolean
@@ -22,6 +23,8 @@ class Succ(n: Nat) extends Nat {
   def isZero = false
   def predecessor = n
   def + (that: Nat) = new Succ(n + that)
-  def - (that: Nat) = new Succ(n - that)
+  def - (that: Nat) =
+    if (that.isZero) this
+    else n - that.predecessor
 }
 
